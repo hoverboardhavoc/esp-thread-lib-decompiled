d45175e5b79880393c8d6061604fe6ff2d8a3e00
blobmaster <blobmaster.crux120@passmail.net>
2021-08-20 20:32:33 +0800

    decompile: upstream 2fb54104644c7016ba817d08cb7536016bb7c839 (2021-08-20) -- 1 files changed
    Upstream commit: https://github.com/espressif/esp-thread-lib/commit/2fb54104644c7016ba817d08cb7536016bb7c839
    
    Upstream subject: update libopenthread_port.a
    
    Archives decompiled: 2

--- decompiled/libopenthread_port/esp_openthread_netif_glue/openthread_netif_transmit.c
+++ decompiled/libopenthread_port/esp_openthread_netif_glue/openthread_netif_transmit.c
@@ -1,8 +1,8 @@
 /*
- * Last changed at upstream commit 852eceaf38f6d6304f3b446a4a26f861389f83be
- * https://github.com/espressif/esp-thread-lib/commit/852eceaf38f6d6304f3b446a4a26f861389f83be
- * Upstream date: 2021-07-06 14:49:24 +0800
- * Upstream subject: openthread: make queue size and partition configurable
+ * Last changed at upstream commit 2fb54104644c7016ba817d08cb7536016bb7c839
+ * https://github.com/espressif/esp-thread-lib/commit/2fb54104644c7016ba817d08cb7536016bb7c839
+ * Upstream date: 2021-08-20 20:32:33 +0800
+ * Upstream subject: update libopenthread_port.a
  * Source: libopenthread_port -> esp_openthread_netif_glue.o -> openthread_netif_transmit
  *
  * (C) Espressif, Apache License 2.0.
@@ -24,31 +24,32 @@ int openthread_netif_transmit(undefined4 param_1,undefined2 param_2)
   if (aiStack_14[0] == 0) {
     otLogCrit(0xc,"-PLAT----: ","Failed to allocate OpenThread message");
     iVar2 = 0x101;
+    goto _L0;
+  }
+  iVar1 = otMessageAppend(param_1,param_2);
+  if (iVar1 == 0) {
+    iVar2 = xQueueGenericSend(s_packet_queue,aiStack_14,0,0);
+    if (iVar2 == 1) {
+      iVar2 = notify_packets_pending();
+      if (iVar2 != 0) {
+        iVar2 = -1;
+      }
+      goto _L0;
+    }
+    otLogCrit(0xc,"-PLAT----: ","Failed to send to Thread netif: packet queue full");
+    iVar2 = 0x101;
   }
   else {
-    iVar1 = otMessageAppend(param_1,param_2);
-    if (iVar1 == 0) {
-      iVar2 = xQueueGenericSend(s_packet_queue,aiStack_14,0,0);
-      if (iVar2 == 1) {
-        iVar2 = notify_packets_pending();
-        if (iVar2 != 0) {
-          iVar2 = -1;
-        }
-      }
-      else {
-        otLogCrit(0xc,"-PLAT----: ","Failed to send to Thread netif: packet queue full");
-        iVar2 = 0x101;
-      }
-    }
-    else {
-      uVar3 = otThreadErrorToString();
-      otLogCrit(0xc,"-PLAT----: ","Failed to copy to OpenThread message: %s",uVar3);
-      iVar2 = 0x101;
-    }
-    if ((iVar1 != 0) && (aiStack_14[0] != 0)) {
-      otMessageFree();
-    }
+    uVar3 = otThreadErrorToString();
+    otLogCrit(0xc,"-PLAT----: ","Failed to copy to OpenThread message: %s",uVar3);
+    iVar2 = 0x101;
+_L0:
+    if (iVar1 == 0) goto _L0;
   }
+  if (aiStack_14[0] != 0) {
+    otMessageFree();
+  }
+_L0:
   esp_openthread_lock_release();
   return iVar2;
 }
