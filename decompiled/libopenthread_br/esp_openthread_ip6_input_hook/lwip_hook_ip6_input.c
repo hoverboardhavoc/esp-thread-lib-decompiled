e653eec49bba23ead9a9c6da915a60be71d6c1b7
blobmaster <blobmaster.crux120@passmail.net>
2022-07-27 19:16:50 +0800

    decompile: upstream 08bb0dcdce207ec830307c27ef1a1063631ff85c (2022-07-27) -- 1 files changed
    Upstream commit: https://github.com/espressif/esp-thread-lib/commit/08bb0dcdce207ec830307c27ef1a1063631ff85c
    
    Upstream subject: br: fix UDP multicast forwarding
    
    Archives decompiled: 2

--- decompiled/libopenthread_br/esp_openthread_ip6_input_hook/lwip_hook_ip6_input.c
+++ decompiled/libopenthread_br/esp_openthread_ip6_input_hook/lwip_hook_ip6_input.c
@@ -1,8 +1,8 @@
 /*
- * Last changed at upstream commit 8d47b585cf4b5e717aa06b2897d27c843fafa343
- * https://github.com/espressif/esp-thread-lib/commit/8d47b585cf4b5e717aa06b2897d27c843fafa343
- * Upstream date: 2022-05-24 22:56:58 +0800
- * Upstream subject: openthread: rebuild the lib with new toolchain
+ * Last changed at upstream commit 08bb0dcdce207ec830307c27ef1a1063631ff85c
+ * https://github.com/espressif/esp-thread-lib/commit/08bb0dcdce207ec830307c27ef1a1063631ff85c
+ * Upstream date: 2022-07-27 19:16:50 +0800
+ * Upstream subject: br: fix UDP multicast forwarding
  * Source: libopenthread_br -> esp_openthread_ip6_input_hook.o -> lwip_hook_ip6_input
  *
  * (C) Espressif, Apache License 2.0.
@@ -13,43 +13,45 @@
 undefined4 lwip_hook_ip6_input(int param_1,int param_2)
 
 {
-  int iVar1;
-  uint uVar2;
+  uint uVar1;
+  int iVar2;
   int iVar3;
   char *pcVar4;
   undefined4 uVar5;
-  char acStack_21 [9];
+  char acStack_21 [13];
   
-  uVar2 = (uint)*(ushort *)(param_1 + 10);
-  iVar1 = *(int *)(param_1 + 4);
-  if (((uVar2 == *(ushort *)(param_1 + 8)) && (0x27 < uVar2)) && (*(char *)(iVar1 + 0x18) == -1)) {
+  uVar1 = (uint)*(ushort *)(param_1 + 8);
+  iVar2 = *(int *)(param_1 + 4);
+  if (((*(ushort *)(param_1 + 10) == uVar1) && (0x27 < uVar1)) &&
+     ((*(uint3 *)(iVar2 + 0x18) & 0xff) == 0xff)) {
+    if (((*(uint *)(iVar2 + 0x18) & 0x8fff) != 0x1ff) && (1 < *(byte *)(iVar2 + 7))) {
+      esp_openthread_multicast_forward_packet();
+    }
     iVar3 = esp_openthread_get_lwip_backbone_netif();
-    if (iVar3 == param_2) {
-      pcVar4 = (char *)skip_ipv6_header_and_extensions(iVar1,uVar2,acStack_21);
+    if (param_2 == iVar3) {
+      pcVar4 = (char *)skip_ipv6_header_and_extensions(iVar2,uVar1,acStack_21);
       if (pcVar4 == (char *)0x0) {
         return 0;
       }
       if (acStack_21[0] != ':') {
         return 0;
       }
-      uVar2 = uVar2 - ((int)pcVar4 - iVar1) & 0xffff;
-      if (uVar2 < 8) {
+      uVar1 = uVar1 - ((int)pcVar4 - iVar2) & 0xffff;
+      if (uVar1 < 8) {
         return 0;
       }
       if ((byte)(*pcVar4 + 0x7dU) < 2) {
-        if (uVar2 < 0x18) {
+        if (uVar1 < 0x18) {
           return 0;
         }
-        handle_mld_input_part_0(iVar1,iVar3);
+        handle_mld_input_part_0(iVar2,param_2);
         return 0;
       }
-      if (*pcVar4 == -0x71) {
-        uVar5 = handle_mldv2_input(iVar1,pcVar4);
-        return uVar5;
+      if (*pcVar4 != -0x71) {
+        return 0;
       }
-    }
-    if (((*(uint *)(iVar1 + 0x18) & 0x8fff) != 0x1ff) && (1 < *(byte *)(iVar1 + 7))) {
-      esp_openthread_multicast_forward_packet(param_1,param_2);
+      uVar5 = handle_mldv2_input(iVar2,pcVar4,param_2);
+      return uVar5;
     }
   }
   return 0;
